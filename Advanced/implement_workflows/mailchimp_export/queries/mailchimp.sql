9054d08d52789b786a0ecfcd515d273275d447e7
Tatsuo Kawasaki <kawasaki@wwing.net>
2021-12-28 18:50:31 +0900

    Updated mailchimp sample workflow

--- Advanced/implement_workflows/mailchimp_export/queries/mailchimp.sql
+++ Advanced/implement_workflows/mailchimp_export/queries/mailchimp.sql
@@ -1,6 +1,4 @@
  SELECT 
-   email,
-   SPLIT_PART(COALESCE(name, 'UNKNOWN UNKNOWN'), ' ', 1) AS fname, 
-   SPLIT_PART(COALESCE(name, 'UNKNOWN UNKNOWN'), ' ', 2) AS lname 
- FROM orders 
- WHERE email IS NOT NULL
+   *
+ FROM
+  l2_mail_data.mailchimp_list
